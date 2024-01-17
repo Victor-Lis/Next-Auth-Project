8e5cc916721ea26eedcecfed98c21c85542c5c4c
Victor-Lis <victorlisbronzo1@gmail.com>
2024-01-17 19:02:34 -0300

    v1.2
    Criando páginas do projeto.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
+    <div className="w-full max-w-screen-xl h-screen flex justify-center items-center">
     <h1>home</h1>
+   </div>
   )
 }
+2
---
+++ src/app/public/page.tsx
@@ -0,0 +1,7 @@
+export default function PublicPage() {
+ return (
+   <div className="w-full max-w-screen-xl h-screen flex justify-center items-center">
+    <h1>public page</h1>
+   </div>
+ );
+}
